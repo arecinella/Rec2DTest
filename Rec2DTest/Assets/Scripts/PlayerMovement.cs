d1586c1cc8adf3ed87f9d20965b9982d9dc9b436
arecinella <96181818+arecinella@users.noreply.github.com>
2022-12-26 21:51:40 +0100

    Movement (not velocity)

---
+++ Rec2DTest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]private int speed = 5;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    private void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMovement(InputValue value){
+        movement = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate(){
+       //test number thirty two
+       if (movement.x !=0 || movement.y !=0){
+ rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+       }
+  
+    //  rb.velocity = movement + speed;
+
+    }
+}
